fba1efbfdcd59f1306c6c36decb537d7e1923d97
Elom Atsou Agboka <opentech.consult@gmail.com>
2019-12-17 14:05:56 +0000

    Combine two middleware components with Connect's use method.
    #4

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const app = require('connect')();
-app.use((req, res, next) => {
-   res.end('Hello World!');
-});
-app.listen(3000);
+const app = require('connect');
+// Prints HTTP method and requests URL and calls next()
+function logger(req, res, next) {
+   console.log('%s %s', req.method, req.url);
+   next();
+}
+
+// Ends response to HTTP request with "hello world"
+function hello(req, res) {
+   res.setHeader('Content-Type', 'text/plain');
+   res.end('hello world');
+}
+connect().use(logger).use(hello).listen(3000);
