122b2ca16cd5fd92b58bfd2f6d0115b00be567b1
Elom Atsou Agboka <opentech.consult@gmail.com>
2019-12-22 16:03:48 +0000

    Generate error and see How Connect will react.
    We have intentionally created a bug and we want to see how Connect will react.
    
    #`

--- hello.js
+++ hello.js
@@ -1,6 +1,7 @@
 // Ends response to HTTP request with "hello world"
-function hello(req, res) {
+function hello(req, res, next) {
     res.setHeader('Content-Type', 'text/plain');
-    res.end('hello world');
+    res.write('Hello World!');
+    next();
  }
  module.exports = hello;
--- index.js
+++ index.js
@@ -3,4 +3,12 @@ const hello = require('./hello');
 const setup = require('./setup');
 
 
-connect().use(setup(':method :url')).use(hello).listen(3000);
+connect()
+.use(setup(':method :url'))
+.use(hello)
+.use((req, res) =>{
+   foo();
+   res.setHeader('Content-Type','text/plain');
+   res.end('End of the game');
+})
+.listen(3000);
