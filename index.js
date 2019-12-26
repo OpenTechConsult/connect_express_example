56b3349804c58bf37f2e2b83adc1854db417f287
Elom Atsou Agboka <opentech.consult@gmail.com>
2019-12-26 10:11:03 +0000

    Use of middleware component with error-handling component
    #8

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const connect = require('connect');
 const hello = require('./hello');
 const setup = require('./setup');
+const errorHandler = require('./errorHandler');
 
 
 connect()
-.use(setup(':method :url'))
-.use(hello)
-.use((req, res) =>{
-   foo();
-   res.setHeader('Content-Type','text/plain');
-   res.end('End of the game');
-})
+.use(router(require('./routes/user')))
+.use(router(require('./routes/blog')))
+.use(router(require('./routes/admin')))
+.use(errorHandler)
+// .use(setup(':method :url'))
+// .use(hello)
+// .use((req, res) =>{
+//    foo();
+//    res.setHeader('Content-Type','text/plain');
+//    res.end('End of the game');
+// })
 .listen(3000);
